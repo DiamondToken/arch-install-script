0e693c79a27f71305f422b1a9c6fb128e313b4c1
Dmitry Elizarov <d.elizarov.00@gmail.com>
2023-01-30 21:59:37 +0300

    quite bad init

---
+++ chroot-install.sh
@@ -0,0 +1,41 @@
+ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+
+hwclock --systohc
+
+sed -i "/ru/s/^#//" "/etc/locale.gen"
+sed -i "/en/s/^#//" "/etc/locale.gen"
+sed -i "/fr/s/^#//" "/etc/locale.gen"
+sed -i "/de/s/^#//" "/etc/locale.gen"
+sed -i "/es/s/^#//" "/etc/locale.gen"
+sed -i "/gr/s/^#//" "/etc/locale.gen"
+
+locale-gen
+
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+echo "$2" > /etc/hostname
+
+mkinitcpio -P
+
+# read -t 1 -n 1000000 discard      # discard previous input
+
+
+if [ "$(ls /sys/firmware/efi/efivars)" ] # uefi
+then
+    grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi $1
+    grub-mkconfig -o /boot/grub/grub.cfg
+else                                     # bios
+    grub-install $1
+    grub-mkconfig -o /boot/grub/grub.cfg
+fi
+
+useradd -m -G wheel -s /bin/zsh $2
+
+echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
+
+echo "root:123" | chpasswd
+echo "$2:$3"    | chpasswd
+
+echo "$2" > /etc/hostname
+
+systemctl enable dhcpcd
---
+++ first.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/sh
+
+
+DRIVE=$1
+
+
+if [ "$(ls /sys/firmware/efi/efivars)" ]
+then
+    parted --script $DRIVE \
+           mklabel gpt \
+           mkpart primary ext4 0% 513MiB \
+           mkpart primary ext4 513MiB 100%
+
+    mkfs.ext4 ${DRIVE}2
+    mount "${DRIVE}2" /mnt
+
+    mkdir -p /mnt/boot/efi
+    mkfs.fat -F32 /dev/${DRIVE}1
+    mount ${DRIVE}1 /mnt/boot/efi
+else
+    parted --script $DRIVE \
+           mklabel msdos \
+           mkpart primary ext4 0% 100%
+
+    mkfs.ext4 ${DRIVE}1
+    mount ${DRIVE}1
+fi
+
+
+pacstrap /mnt base base-devel linux linux-firmware vim grub efibootmgr os-prober dhcpcd zsh
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+cp chroot-install.sh /mnt/
+arch-chroot /mnt "/bin/bash" "./chroot-install.sh" "$DRIVE" diamond 1234
